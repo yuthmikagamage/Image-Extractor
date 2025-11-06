56e27e4753c5e7cf04c9b76cda48a8e221fe07b6
yuthmikagamage <vanujyuthmika@gmail.com>
2025-11-06 20:46:55 +0530

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,10 @@
+document.getElementById("extractBtn").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: extractImages,
+  });
+});
+
+function extractImages() {}
